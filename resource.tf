81e8b9d19d03d8130e1eddbacb17933804dbc431
Saikumar-Pamulaparthi <142103256+Saikumar-Pamulaparthi@users.noreply.github.com>
2025-10-13 12:33:20 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "Dev" {
+    ami = "ami-052064a798f08f0d3"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Dev"
+    }
+}
